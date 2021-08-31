715ee910290a671ded049e6906e1fef615886e0e
sunmengyue <mengyuesun@gmail.com>
2021-08-31 00:49:41 -0400

    create schemas

---
+++ backend/models/productModel.js
@@ -0,0 +1,59 @@
+import { Decimal128 } from 'bson';
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  { timeStamps: true },
+);
+
+const productSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  reviews: [reviewSchema],
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const Product = ('Product', productSchema);
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import products from './data/products.js';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -23,5 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`),
+  console.log(
+    `server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold,
+  ),
 );
