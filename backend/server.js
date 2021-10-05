91f1d69b1cb8bd4fe517bba1082f4d1a9a7a29be
sunmengyue <mengyuesun@gmail.com>
2021-10-05 18:42:41 -0400

    add node version to package.json

--- backend/server.js
+++ backend/server.js
@@ -28,12 +28,13 @@ app.use(errorHandler);
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
   );
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running......');
+    res.send('API is running....');
   });
 }
 
