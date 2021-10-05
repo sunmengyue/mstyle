08b3e239611e5b0ab11d0d0f45b48354dde03ece
sunmengyue <mengyuesun@gmail.com>
2021-10-05 17:29:05 -0400

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import colors from 'colors';
 import connectDB from './config/db.js';
@@ -14,10 +15,6 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running......');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -27,6 +24,18 @@ app.get('/api/config/paypal', (req, res) =>
 );
 app.use(notFound);
 app.use(errorHandler);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running......');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
