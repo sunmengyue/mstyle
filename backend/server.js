3c4588b6892d6fcf7a899485917dd3685fbd593c
sunmengyue <mengyuesun@gmail.com>
2021-10-05 20:01:33 -0400

    edit server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,23 +22,22 @@ app.use('/api/orders', orderRoutes);
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID),
 );
-app.use(notFound);
-app.use(errorHandler);
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'),
-    ),
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
   );
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
 }
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
