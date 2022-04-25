74370c42d68fd3a96282386212bbdf31fb33ab7a
sunmengyue <mengyuesun@gmail.com>
2022-04-25 13:26:25 -0400

    add logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useHistory } from "react-router-dom";
-import Search from "./Search";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import Search from './Search';
 import {
   MenuIcon,
   SearchIcon,
   ShoppingBagIcon,
   XIcon
-} from "@heroicons/react/outline";
-import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
-import { logout } from "../actions/userActions";
-import MenuExpand from "./MenuExpand";
+} from '@heroicons/react/outline';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
+import { logout } from '../actions/userActions';
+import MenuExpand from './MenuExpand';
 
 const Navbar = () => {
   const [showSearchBar, setShowSearchBar] = useState(false);
@@ -19,7 +19,7 @@ const Navbar = () => {
 
   const history = useHistory();
   const toCart = () => {
-    history.push("/cart");
+    history.push('/cart');
   };
 
   const dispatch = useDispatch();
@@ -44,7 +44,7 @@ const Navbar = () => {
   };
 
   const toProfile = () => {
-    history.push("/profile");
+    history.push('/profile');
     setShowDropdown(false);
   };
   const logoutHandler = () => {
@@ -52,64 +52,66 @@ const Navbar = () => {
     setShowDropdown(false);
   };
   const toAllUsers = () => {
-    history.push("/admin/users");
+    history.push('/admin/users');
     setShowDropdown(false);
   };
   const toProductList = () => {
-    history.push("/admin/productList");
+    history.push('/admin/productList');
     setShowDropdown(false);
   };
 
   return (
-    <div className="bg-brown-light px-10 top-0 sticky z-50">
-      <nav className="py-5 ">
-        <div className="max-w-6xl m-auto flex justify-between items-center ">
+    <div className='bg-brown-light px-10 top-0 sticky z-50'>
+      <nav className='py-5 '>
+        <div className='max-w-6xl m-auto flex justify-between items-center '>
           {/* left */}
           <div>
-            <div className="flex justify-between md:hidden">
+            <div className='flex justify-between md:hidden'>
               <button onClick={toggleExpand}>
                 {!expandMenu ? (
-                  <MenuIcon className="h-6 mr-2 cursor-pointer" />
+                  <MenuIcon className='h-6 mr-2 cursor-pointer' />
                 ) : (
-                  <XIcon className="h-6 mr-5" />
+                  <XIcon className='h-6 mr-5' />
                 )}
               </button>
               <button onClick={toggleSearchBar}>
-                <SearchIcon className="h-6 cursor-pointer" />
+                <SearchIcon className='h-6 cursor-pointer' />
               </button>
             </div>
-            <ul className="hidden md:flex justify-between items-center font-semibold space-x-8 text-sm">
-              <li className="link">
+            <ul className='hidden md:flex justify-between items-center font-semibold space-x-8 text-sm'>
+              <li className='link'>
                 <button onClick={toggleSearchBar}>
-                  <SearchIcon className="h-6" />
+                  <SearchIcon className='h-6' />
                 </button>
               </li>
-              <Link to="/shopAll">
-                <li className="link tracking-wide">Shop All</li>
+              <Link to='/shopAll'>
+                <li className='link tracking-wide'>Shop All</li>
               </Link>
             </ul>
           </div>
           {/* Middle */}
-          <div className="cursor-pointer">
-            <h4 className="uppercase font-bold tracking-wider">
-              <Link to="/">mstyle</Link>
+          <div className='cursor-pointer'>
+            <h4 className='uppercase font-bold tracking-wider'>
+              <Link to='/'>
+                <img src='/images/logo.png' />
+              </Link>
             </h4>
           </div>
           {/* right */}
-          <div className="relative md:hidden" onClick={toCart}>
-            <ShoppingBagIcon className="h-6 cursor-pointer" />
-            <p className="absolute -top-2 right-0 text-xs">
+          <div className='relative md:hidden' onClick={toCart}>
+            <ShoppingBagIcon className='h-6 cursor-pointer' />
+            <p className='absolute -top-2 right-0 text-xs'>
               {showCartItemsNo()}
             </p>
           </div>
-          <ul className="hidden md:flex justify-between items-center font-semibold space-x-8 text-sm">
+          <ul className='hidden md:flex justify-between items-center font-semibold space-x-8 text-sm'>
             {userInfo ? (
-              <div className="flex flex-col">
-                <li className="uppercase flex">
-                  <p className="mr-1">{userInfo.name}</p>
+              <div className='flex flex-col'>
+                <li className='uppercase flex'>
+                  <p className='mr-1'>{userInfo.name}</p>
                   {!showDropdown ? (
                     <ChevronDownIcon
-                      className="h-5 cursor-pointer"
+                      className='h-5 cursor-pointer'
                       onClick={() => {
                         setShowDropdown(true);
                       }}
@@ -119,39 +121,39 @@ const Navbar = () => {
                       onClick={() => {
                         setShowDropdown(false);
                       }}
-                      className="h-5 cursor-pointer"
+                      className='h-5 cursor-pointer'
                     />
                   )}
                 </li>
                 {showDropdown && (
-                  <ul className="absolute mt-6 bg-brown-light p-5 space-y-4 ">
+                  <ul className='absolute mt-6 bg-brown-light p-5 space-y-4 '>
                     {userInfo.isAdmin && (
-                      <li className="link" onClick={toAllUsers}>
+                      <li className='link' onClick={toAllUsers}>
                         User List
                       </li>
                     )}
                     {userInfo.isAdmin && (
-                      <li className="link" onClick={toProductList}>
+                      <li className='link' onClick={toProductList}>
                         Product List
                       </li>
                     )}
-                    <li className="link" onClick={toProfile}>
+                    <li className='link' onClick={toProfile}>
                       Profile
                     </li>
 
-                    <li className="link" onClick={logoutHandler}>
+                    <li className='link' onClick={logoutHandler}>
                       Logout
                     </li>
                   </ul>
                 )}
               </div>
             ) : (
-              <Link to="/login">
-                <li className="link">Sign In</li>
+              <Link to='/login'>
+                <li className='link'>Sign In</li>
               </Link>
             )}
 
-            <li className="link" onClick={toCart}>
+            <li className='link' onClick={toCart}>
               Bag ({showCartItemsNo()})
             </li>
           </ul>
